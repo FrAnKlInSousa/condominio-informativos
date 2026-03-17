417ff08274048e5b9efa596b28c89171f76a170f
franklin <frank.fkn.23@gmail.com>
2026-03-17 16:05:53 -0300

    definindo a rota do componente comunicados

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { Comunicados } from './comunicados/comunicados';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: Comunicados }];
